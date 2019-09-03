5dd3fc6800e8c25759f09c4266714b54341608dc
Ved,Shubhra <Shubhra.Ved@cerner.com>
2019-09-03 22:41:24 +0530

    Added changes

--- FactorialWithTestCases/FactorialMain.java
+++ FactorialWithTestCases/FactorialMain.java
@@ -4,7 +4,7 @@ public class FactorialMain {
 	public static void main(String[] args) {
 		Factorial factObj = new Factorial();
 		
-		int res = factObj.calculateFactorial(-7);
+		int res = factObj.calculateFactorial(7);
 		System.out.println("The factorial is:"+ res);
 
 	}
